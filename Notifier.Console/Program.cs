44f744e2c73074b7b9f632cc06aa460134d6c507
jhosm <joao.hugo.miranda@gmail.com>
2024-07-28 18:09:50 +0100

    Initial Commit

---
+++ Notifier.Console/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var notifier = new Notifier.Core.Notifier();
+
+Console.WriteLine(notifier.SayHello());
---
+++ Notifier.Core/Notifier.cs
@@ -0,0 +1,9 @@
+﻿namespace Notifier.Core;
+
+public class Notifier
+{
+    public string SayHello() {
+        return "Hey!";
+    }
+
+}
